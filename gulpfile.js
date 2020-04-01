c239b3d43cd830a00634ea7eeaa2113270292d76
SichinavaVZ <straikvov@gmail.com>
2020-04-01 10:32:55 +0300

    no message

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+// compile scss into css
+function style(){
+	//1. where is my scss file
+	return gulp.src('app/scss/**/*.scss')
+	//2. pass that file through sass compiler
+	.pipe(sass().on('error', sass.logError))
+	//3. where do I save the compiled css?
+	.pipe(gulp.dest('app/css'))
+	//4. stream changes to browsers
+	.pipe(browserSync.stream());
+	
+}
+
+function watch(){
+	browserSync.init({
+		server: {
+			baseDir: 'app/'
+		}
+	});
+	gulp.watch('app/scss/**/*.scss', style);
+	gulp.watch('app/*.html').on('change', browserSync.reload);
+	gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
+
+}
+exports.watch = watch;
+exports.style = style;
